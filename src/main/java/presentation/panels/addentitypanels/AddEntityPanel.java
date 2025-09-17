18e4f4b77f21d546d8720a5ad9e7aae0d9f5ba5e
BananaFarman <david_ec22@hotmail.com>
2025-09-17 08:39:28 -0700

    AddEntityPanels

--- src/main/java/presentation/panels/addentitypanels/AddEntityPanel.java
+++ src/main/java/presentation/panels/addentitypanels/AddEntityPanel.java
@@ -22,17 +22,6 @@ public class AddEntityPanel extends JPanel {
         this.mainPanel = new JPanel();
         this.mainPanel.setLayout(new BoxLayout(this.mainPanel, BoxLayout.Y_AXIS));
         this.mainPanel.setOpaque(false);
-        this.componentsPanel = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                Graphics2D g2d = (Graphics2D) g.create();
-                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                g2d.setColor(Style.COLOR_BACKGROUND);
-                g2d.fillRoundRect(0, 0, getWidth(), getHeight(), 20, 20);
-                g2d.dispose();
-            }
-        };
         this.buttonsPanel = new JPanel();
         this.addBtn = new Button("Agregar", 100, 35, 15, 25, Color.WHITE, Style.COLOR_BTN, Style.COLOR_BTN_HOVER);
         this.backBtn = new Button("←", 50, 35, 15, 25, Color.WHITE, Style.COLOR_BTN_BACK, Style.COLOR_BTN_BACK_HOVER);
